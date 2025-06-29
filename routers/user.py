80e81c2791e3e4f2da7502af9074938fb59bae6e
Harsh Kumar <hadron43@yahoo.com>
2025-06-29 16:35:03 +0530

    feat: user APIs

--- routers/user.py
+++ routers/user.py
@@ -1,8 +1,53 @@
-from fastapi import APIRouter, FastAPI
+from fastapi import APIRouter, Depends, HTTPException
+from sqlmodel import Session, select
+from typing import List
 
-base_router = APIRouter()
+from database import get_session
+from models.users import User, UserCreate, UserRead, UserUpdate
 
-# Health API
-@base_router.get("/health")
-def health_check():
-    return {"status": "ok"}
+user = APIRouter(prefix='/user', tags=['users'])
+
+@user.post("/", response_model=UserRead)
+def create_user(*, session: Session = Depends(get_session), user: UserCreate):
+    db_user = User.model_validate(user)
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+    return db_user
+
+@user.get("/", response_model=List[UserRead])
+def read_users(*, session: Session = Depends(get_session), skip: int = 0, limit: int = 100):
+    users = session.exec(select(User).offset(skip).limit(limit)).all()
+    return users
+
+@user.get("/{user_id}", response_model=UserRead)
+def read_user(*, session: Session = Depends(get_session), user_id: int):
+    user = session.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+@user.patch("/{user_id}", response_model=UserRead)
+def update_user(*, session: Session = Depends(get_session), user_id: int, user: UserUpdate):
+    db_user = session.get(User, user_id)
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    user_data = user.model_dump(exclude_unset=True)
+    for key, value in user_data.items():
+        setattr(db_user, key, value)
+
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+    return db_user
+
+@user.delete("/{user_id}", status_code=204)
+def delete_user(*, session: Session = Depends(get_session), user_id: int):
+    user = session.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    session.delete(user)
+    session.commit()
+    return None
